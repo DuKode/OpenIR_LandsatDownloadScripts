4844d3c50d306f91fdf133abe1ca0786a517c3f4
ilia koen <ilias.koen@dukodestudio.com>
2012-07-01 01:22:47 -0400

    added menu for selecting the image to be downloaded

--- getTilewithPathAndRow.sh
+++ getTilewithPathAndRow.sh
@@ -37,30 +37,30 @@ echo $TILEDOWNLOADPATH
 #################################################################################
 echo "#################################"
 echo "PREPARING LOCAL TEMPORARY FOLDER"
-echo " "
+#echo " "
 if [ -d $TEMPDIR ]; then 
    
 	echo "temporary directory already exists! thanks for asking..."
 	#################################################################################
 	#empty tmp directory before continuing 
 	#################################################################################
-	echo $(ls -A $TEMPDIR)
+	#echo $(ls -A $TEMPDIR) #print temp directory files
 	if [ "$(ls -A $TEMPDIR)" ]; then
 
-	
-	    # do dangerous stuff
-		echo " "
-		read -p "Are you sure you want to delete the data from temporary directory? (y or n) " -n 1 -r
-		echo " "
-		if [[ $REPLY =~ ^[Yy]$ ]];then
+		#commented out the checks for the temp directory - now it just deletes the content here
+		# 	    # do dangerous stuff
+		# echo " "
+		# read -p "Are you sure you want to delete the data from temporary directory? (y or n) " -n 1 -r
+		# echo " "
+		# if [[ $REPLY =~ ^[Yy]$ ]];then
 			echo "Emptying temporary directory...."
 			rm $TEMPDIR/*
 			echo "Done"
-		else
-			echo "$TEMPDIR is Empty"
-		fi
-	else
-		echo "The temporary directory content if any will not be deleted"
+	# 	else
+	# 		echo "$TEMPDIR is Empty"
+	# 	fi
+	# else
+	# 	echo "The temporary directory content if any will not be deleted"
 	fi
 
 	
@@ -69,27 +69,58 @@ else
 	echo "Created temporary directory for data storage..."
 fi
 
+echo "#### "
 echo " "
-echo "lookup FTP directory" $TILEDOWNLOADPATH
-read -p "Press [Enter] key to start lookup FTP directory... FTP directory: $TILEDOWNLOADPATH"
+# echo "lookup FTP directory" $TILEDOWNLOADPATH
+# read -p "Press [Enter] key to start lookup FTP directory... FTP directory: $TILEDOWNLOADPATH"
 #################################################################################
 
+#
 wget -rq -nd --no-parent -P $TEMPDIR $TILEDOWNLOADPATH 
 #wget -r -nd --no-parent -P $TEMPDIR $TILEDOWNLOADPATH 
 cd $TEMPDIR/
 
 FILES=*
-
+#count array
 TILEFILESARRAY=(`echo $FILES`)
+echo "#################################"
+echo "SELECT FILE TO DOWNLOAD\n"
+n=-1
+for i in ${TILEFILESARRAY[@]}; do
+ 	echo "["."$(( n += 1 ))"."]" " $i"
+ done
+TILEFILESARRAYlength="$n"
 
-# for i in ${TILEFILESARRAY[@]}; do
-# 	echo "$i"
-# done
+#check if row and path have corresponding images assosicated to them
+if [ $TILEFILESARRAYlength == "0" ]; then 
+	echo "Invalid path and row. exit "
+	rm -rf -- $TEMPDIR #remove temp directory 
+	exit 0 #exit 
+fi
 
+echo; echo "Hit a number between 0 - $TILEFILESARRAYlength, then hit return."
+
+selection=
+until [ "$selection" = "0" ]; do
+	read keypress
+	case "$keypress" in
+		[0-9] ) 
+			echo "Pending statement for the validity of the entry... TO DO"
+			SELECTEDINDEX="$keypress"
+			TILEDOWNLOADPATH="ftp://ftp.glcf.umd.edu/glcf/Landsat/WRS2/p"$TILEPATH"/r"$TILEROW"/${TILEFILESARRAY[SELECTEDINDEX]}/*"
+			
+			#elaborate on code to check if directory is valid.
+			#wget ftp://ftp.glcf.umd.edu/glcf/Landsat/WRS2/p020/r032/p020r032_7dx20010821.ETM-GLS2000/* && echo exists || echo not exist
+			#wget --progress=dot $TILEDOWNLOADPATH && echo exist || echo not exist
+			break
+	 	;;
+		* ) echo "Please enter a valid selection"
+	esac
+done
 cd .. 
 
-echo " "
-read -p "Press [Enter] key to download the first file"
+echo "SELECTED FILE ------ ${TILEFILESARRAY[$SELECTEDINDEX]} "
+#read -p "Press [Enter] key to download the first file"
 #################################################################################
 
 
@@ -101,24 +132,58 @@ read -p "Press [Enter] key to download the first file"
 #################################################################################
 #download data
 #################################################################################
-TILEDOWNLOADPATH="ftp://ftp.glcf.umd.edu/glcf/Landsat/WRS2/p"$TILEPATH"/r"$TILEROW"/${TILEFILESARRAY[0]}/*"
-
+echo "#### "
+echo " "
 echo "#################################"
 echo "DOWNLOAD DATA\n"
-echo " "
-echo $TILEDOWNLOADPATH
-echo " "
+#echo " "
+echo "Downloading tile from path >>>> $TILEDOWNLOADPATH"
 
-TILENAME=${TILEFILESARRAY[0]}
-
-read -p "Are you sure you want to download the data? (y or n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # do dangerous stuff
-	wget -r -nd --no-parent -P $TEMPDIR $TILEDOWNLOADPATH 
-	
-	echo "\nfinished directory files download"
-	read -p "Press [Enter] key to continue"
+TILENAME=${TILEFILESARRAY[$SELECTEDINDEX]}
+#ARCHIVEPATH="p$TILEPATH"."r$TILEROW"
+ARCHIVEPATH="${TILEFILESARRAY[$SELECTEDINDEX]}"
+#download new data - create archive with downloaded data p.r and look for archive if no is selected. 
+		
+if [ -d $ARCHIVEPATH ]; then 
+	echo "The selected file >>>> $TILENAME data was found in archive! No download necessary...skip that!"
+	echo "copying files from $ARCHIVEPATH to $TEMPDIR " 
+	cp $ARCHIVEPATH/* $TEMPDIR/
+else
+	read -p "Are you sure you want to download the data? (y or n) " -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	    # do dangerous stuff
+		#quiet
+		wget -r -nd --no-parent -P $TEMPDIR $TILEDOWNLOADPATH 
+		
+		echo "\nfinished directory files download"
+		
+		#clean up temp directory of the link files.
+		#clean up temp directory of the link files.
+		echo "Clean up temp directory of the temp ftp link files."
+		n=-1
+		for i in ${TILEFILESARRAY[@]}; do
+			echo "deleting temp files"."$(( n += 1 ))"."" " $i"
+		 	rm "$TEMPDIR/${TILEFILESARRAY[$n]}"
+		done
+		echo "Done"
+		echo "#### "
+		#####
+		
+		read -p "Press [Enter] key to continue"
+		#CREATE A NEW DIRECTORY WITH AN ARCHIVE OF THE DOWNLOADED FILES.
+		echo "#################################"
+		echo "ARCHIVE DOWNLOADED DATA\n"
+		echo "making directory $ARCHIVEPATH"
+		mkdir "$ARCHIVEPATH" 
+		echo "copying files from $TEMPDIR to $ARCHIVEPATH" 
+		cp $TEMPDIR/* $ARCHIVEPATH/
+		echo "Done"
+		echo "#### "
+		echo " "
+	else
+		echo "\nNo data exist on the archive. exit"
+		exit 0
+	fi
 fi
 
 
@@ -132,61 +197,62 @@ RESULT="found"
 for i in ${TILEFILESARRAY[@]}; do
 	if [[ "$i" == *B10* ]]; then
 		BAND10=$i
-		echo "$BAND10 exists!"
+		#echo "$BAND10 exists!"
 	fi
 	#B20
 	if [[ "$i" == *B20* ]]; then
 		BAND20=$i
-		echo "$BAND20 exists!"
+		#echo "$BAND20 exists!"
 	fi
 	#B30
 	if [[ "$i" == *B30* ]]; then
 		BAND30=$i
-		echo "$BAND30 exists!"
+		#echo "$BAND30 exists!"
 	fi
 	#B40
 	if [[ "$i" == *B40* ]]; then
 		BAND40=$i
-		echo "$BAND40 exists!"
+		#echo "$BAND40 exists!"
 	fi
 	
 	#B50
 	if [[ "$i" == *B50* ]]; then
 		BAND50=$i
-		echo "$BAND50 exists!"
+		#echo "$BAND50 exists!"
 	fi
 	
 	#B60
 	if [[ "$i" == *B60* ]]; then
 		BAND60=$i
-		echo "$BAND60 exists!"
+		#echo "$BAND60 exists!"
 	fi
 	
 	#B61
 	if [[ "$i" == *B61* ]]; then
 		BAND61=$i
-		echo "$BAND61 exists!"
+		#echo "$BAND61 exists!"
 	fi
 	
 	#B62
 	if [[ "$i" == *B62* ]]; then
 		BAND62=$i
-		echo "$BAND62 exists!"
+		#echo "$BAND62 exists!"
 	fi
 	
 	#B70
 	if [[ "$i" == *B70* ]]; then
 		BAND70=$i
-		echo "$BAND70 exists!"
+		#echo "$BAND70 exists!"
 	fi
 	
 	#B80
 	if [[ "$i" == *B80* ]]; then
 		BAND80=$i
-		echo "$BAND80 exists!"
+	#	echo "$BAND80 exists!"
 	fi
 done
-
+echo "#### "
+echo " "
 echo "#################################"
 echo "UNZIPING FILES"
 echo " "
@@ -273,9 +339,12 @@ fi
 #################################################################################
 # GENERATE COMPOSITES
 #################################################################################
+echo "#### "
+echo " "
 echo "#################################"
 echo "GENERATE COMPOSITES"
 echo " "
+
 #use for surface reflectance 
 #convert ${args[0]} ${args[1]} ${args[2]} -combine -level 0.0%x8% ${args[3]}
 
@@ -283,79 +352,261 @@ echo " "
 ## add if statement if bands exist. 
 ## append stuff to log
 
-#use for GLS2005
-convert -monitor $BAND30TIF $BAND20TIF $BAND10TIF -combine $TILENAME".321.TIF"
-echo 'from 16 to 8bit'
-convert -monitor $TILENAME".321.TIF" -depth 8 $TILENAME".8bit.321.TIF"
 
-rm  $TILENAME".321.TIF"
+echo "create processed archive"
+PROCESSEDDIR="$TILENAME.processed"
+cd ..
+ls -A .
+if [ -d $PROCESSEDDIR ]; then
+ 	cp $PROCESSEDDIR/* $TMPDIR/
+else
+echo "create processed archive1"
+	mkdir $PROCESSEDDIR
+  	echo "cd $TEMPDIR/"
+  	cd $TEMPDIR/
+#echo "create processed archive2"
+	ls -A .
+#echo "create processed archive3"	
 
-
-convert -monitor $BAND40TIF $BAND30TIF $BAND20TIF -combine $TILENAME".432.TIF"
-echo 'from 16 to 8bit'
-convert -monitor $TILENAME".432.TIF" -depth 8 $TILENAME".8bit.432.TIF"
-rm  $TILENAME".432.TIF"
-
-
-convert -monitor $BAND50TIF $BAND40TIF $BAND30TIF -combine $TILENAME".543.TIF"
-echo 'from 16 to 8bit'
-convert -monitor $TILENAME".543.TIF" -depth 8 $TILENAME".8bit.543.TIF"
-rm $TILENAME".543.TIF"
- 
-
-convert -monitor $BAND40TIF $BAND50TIF $BAND30TIF -combine $TILENAME".453.TIF"
-echo 'from 16 to 8bit'
-convert -monitor $TILENAME".453.TIF" -depth 8 $TILENAME".8bit.453.TIF"
-rm  $TILENAME".453.TIF" 
+	read -p "Process all band combinations(321, 432, 543, 453, 745)? /Yes/ will process all / /No/ I want to select specific bands(y or n) " -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 
-convert -monitor $BAND70TIF $BAND50TIF $BAND40TIF -combine $TILENAME".754.TIF"
-echo 'from 16 to 8bit'
-convert -monitor $TILENAME".754.TIF" -depth 8 $TILENAME".8bit.754.TIF"
-rm $TILENAME".754.TIF" 
- 
+		echo "done"
 
-##########################################
-# // apply georeference to composites // #
-##########################################
-listgeo $BAND10TIF > $BAND10".txt" 
+		#########################################process all 
 
-geotifcp -g $BAND10".txt"  $TILENAME".8bit.321.TIF" "321.TIF"
-geotifcp -g $BAND10".txt"  $TILENAME".8bit.432.TIF" "432.TIF"
-geotifcp -g $BAND10".txt"  $TILENAME".8bit.543.TIF" "543.TIF"
-geotifcp -g $BAND10".txt"  $TILENAME".8bit.453.TIF" "453.TIF"
-geotifcp -g $BAND10".txt"  $TILENAME".8bit.754.TIF" "754.TIF"
-
-############################################
-#generate alpha information for nodata areas.  
-gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "321.TIF" "321.alpha.TIF"
-gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "432.TIF" "432.alpha.TIF"
-gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "543.TIF" "543.alpha.TIF"
-gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "453.TIF" "453.alpha.TIF"
-gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "754.TIF" "754.alpha.TIF"
-
-rm $TILENAME"321.TIF"
-rm $TILENAME"432.TIF"
-rm $TILENAME"543.TIF"
-rm $TILENAME"453.TIF"
-rm $TILENAME"754.TIF"
-
-mv $TILENAME"321.alpha.TIF" "321.TIF"
-mv $TILENAME"432.alpha.TIF" "432.TIF"
-mv $TILENAME"543.alpha.TIF" "543.TIF"
-mv $TILENAME"453.alpha.TIF" "453.TIF"
-mv $TILENAME"754.alpha.TIF" "754.TIF"
-############################################
+		#use for GLS2005
+		convert -monitor $BAND30TIF $BAND20TIF $BAND10TIF -combine $TILENAME".321.TIF"
+		echo 'from 16 to 8bit'
+		convert -monitor $TILENAME".321.TIF" -depth 8 $TILENAME".8bit.321.TIF"
+		rm  $TILENAME".321.TIF"
 
 
-rm $TILENAME".8bit.321.TIF"
-rm $TILENAME".8bit.432.TIF"
-rm $TILENAME".8bit.543.TIF"
-rm $TILENAME".8bit.453.TIF"
-rm $TILENAME".8bit.754.TIF"
+		convert -monitor $BAND40TIF $BAND30TIF $BAND20TIF -combine $TILENAME".432.TIF"
+		echo 'from 16 to 8bit'
+		convert -monitor $TILENAME".432.TIF" -depth 8 $TILENAME".8bit.432.TIF"
+		rm  $TILENAME".432.TIF"
 
 
 
+		convert -monitor $BAND50TIF $BAND40TIF $BAND30TIF -combine $TILENAME".543.TIF"
+		echo 'from 16 to 8bit'
+		convert -monitor $TILENAME".543.TIF" -depth 8 $TILENAME".8bit.543.TIF"
+		rm $TILENAME".543.TIF"
+
+
+
+		convert -monitor $BAND40TIF $BAND50TIF $BAND30TIF -combine $TILENAME".453.TIF"
+		echo 'from 16 to 8bit'
+		convert -monitor $TILENAME".453.TIF" -depth 8 $TILENAME".8bit.453.TIF"
+		rm  $TILENAME".453.TIF" 
+
+
+		convert -monitor $BAND70TIF $BAND50TIF $BAND40TIF -combine $TILENAME".754.TIF"
+		echo 'from 16 to 8bit'
+		convert -monitor $TILENAME".754.TIF" -depth 8 $TILENAME".8bit.754.TIF"
+		rm $TILENAME".754.TIF" 
+
+		echo "done"
+		echo "#### "
+		echo " "
+
+		# ##########################################
+		# // apply georeference to composites based on the compiled bands selection// #
+		##########################################
+		echo "#################################"
+		echo "Georeference and Alpha"
+		echo " "
+		listgeo $BAND10TIF > $BAND10".txt" 
+		echo "TILENAME:"."$TILENAME"
+
+		#########################################process all
+
+		geotifcp -g $BAND10".txt"  $TILENAME".8bit.321.TIF" "$TILENAME.321.TIF"
+		rm $TILENAME".8bit.321.TIF"
+		# ############################################
+		# #generate alpha information for nodata areas.  
+		# echo ''
+		gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.321.TIF" "$TILENAME.321.alpha.TIF"
+
+
+		geotifcp -g $BAND10".txt"  $TILENAME".8bit.432.TIF" "$TILENAME.432.TIF"
+		rm $TILENAME".8bit.432.TIF"
+		# ############################################
+		# #generate alpha information for nodata areas.  
+		# echo ''
+		gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.432.TIF" "$TILENAME.432.alpha.TIF"
+
+
+		geotifcp -g $BAND10".txt"  $TILENAME".8bit.543.TIF" "$TILENAME.543.TIF"
+		rm $TILENAME".8bit.543.TIF"
+		# ############################################
+		# #generate alpha information for nodata areas.  
+		# echo ''
+		gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.543.TIF" "$TILENAME.543.alpha.TIF"
+
+
+
+		geotifcp -g $BAND10".txt"  $TILENAME".8bit.453.TIF" "$TILENAME.453.TIF"
+		rm $TILENAME".8bit.453.TIF"
+		# ############################################
+		# #generate alpha information for nodata areas.  
+		# echo ''
+		gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.453.TIF" "$TILENAME.453.alpha.TIF"
+
+
+
+		geotifcp -g $BAND10".txt"  $TILENAME".8bit.754.TIF" "$TILENAME.754.TIF"
+		rm $TILENAME".8bit.754.TIF"
+		# ############################################
+		# #generate alpha information for nodata areas.  
+		# echo ''
+		gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.754.TIF" "$TILENAME.754.alpha.TIF"
+
+
+		### rename files for next step. 
+		echo "renamingfiles"
+		cp "$TILENAME.321.alpha.TIF" "321.TIF"
+		cp "$TILENAME.432.alpha.TIF" "432.TIF"
+		cp "$TILENAME.543.alpha.TIF" "543.TIF"
+		cp "$TILENAME.453.alpha.TIF" "453.TIF"
+		cp "$TILENAME.754.alpha.TIF" "754.TIF"
+
+		echo "done"
+		echo "#### "
+		echo "moving files to processed archive"
+		cd ..
+		cp "$TEMPDIR/$TILENAME.321.alpha.TIF" "$PROCESSEDDIR/321.TIF"
+		cp "$TEMPDIR/$TILENAME.432.alpha.TIF" "$PROCESSEDDIR/432.TIF"
+		cp "$TEMPDIR/$TILENAME.543.alpha.TIF" "$PROCESSEDDIR/543.TIF"
+		cp "$TEMPDIR/$TILENAME.453.alpha.TIF" "$PROCESSEDDIR/453.TIF"
+		cp "$TEMPDIR/$TILENAME.754.alpha.TIF" "$PROCESSEDDIR/754.TIF"
+		cd $TEMPDIR
+		echo "done"
+	else
+		echo "commented out for test"
+		# ########################################process selection 
+		# echo ""
+		# 
+		# flag321=0
+		# flag432=0
+		# flag543=0
+		# flag453=0
+		# flag754=0
+		# 
+		# read -p "Compile 321 ? (y or n) " -n 1 -r
+		# if [[ $REPLY =~ ^[Yy]$ ]]; then
+		# echo ""
+		# convert -monitor $BAND30TIF $BAND20TIF $BAND10TIF -combine $TILENAME".321.TIF"
+		# echo 'from 16 to 8bit'
+		# convert -monitor $TILENAME".321.TIF" -depth 8 $TILENAME".8bit.321.TIF"
+		# rm  $TILENAME".321.TIF"
+		# $flag321=1
+		# fi
+		# 
+		# read -p "Compile 432 ? (y or n) " -n 1 -r
+		# if [[ $REPLY =~ ^[Yy]$ ]]; then
+		# echo ""
+		# convert -monitor $BAND40TIF $BAND30TIF $BAND20TIF -combine $TILENAME".432.TIF"
+		# echo 'from 16 to 8bit'
+		# convert -monitor $TILENAME".432.TIF" -depth 8 $TILENAME".8bit.432.TIF"
+		# rm  $TILENAME".432.TIF"
+		# fi
+		# 
+		# read -p "Compile 543 ? (y or n) " -n 1 -r
+		# if [[ $REPLY =~ ^[Yy]$ ]]; then
+		# echo ""
+		# convert -monitor $BAND50TIF $BAND40TIF $BAND30TIF -combine $TILENAME".543.TIF"
+		# echo 'from 16 to 8bit'
+		# convert -monitor $TILENAME".543.TIF" -depth 8 $TILENAME".8bit.543.TIF"
+		# rm $TILENAME".543.TIF"
+		# $flag543=1
+		# fi 
+		# 
+		# read -p "Compile 453 ? (y or n) " -n 1 -r
+		# if [[ $REPLY =~ ^[Yy]$ ]]; then
+		# echo ""
+		# convert -monitor $BAND40TIF $BAND50TIF $BAND30TIF -combine $TILENAME".453.TIF"
+		# echo 'from 16 to 8bit'
+		# convert -monitor $TILENAME".453.TIF" -depth 8 $TILENAME".8bit.453.TIF"
+		# rm  $TILENAME".453.TIF" 
+		# $flag453=1
+		# fi
+		# 
+		# read -p "Compile 754 ? (y or n) " -n 1 -r
+		# if [[ $REPLY =~ ^[Yy]$ ]]; then
+		# echo ""
+		# convert -monitor $BAND70TIF $BAND50TIF $BAND40TIF -combine $TILENAME".754.TIF"
+		# echo 'from 16 to 8bit'
+		# convert -monitor $TILENAME".754.TIF" -depth 8 $TILENAME".8bit.754.TIF"
+		# rm $TILENAME".754.TIF" 
+		# $flag754=1
+		# fi
+		# ##########################################
+		# # // apply georeference to composites based on the compiled bands selection// #
+		# ##########################################
+		# listgeo $BAND10TIF > $BAND10".txt" 
+		# echo "TILENAME:"."$TILENAME"
+		# 
+		# #########################################process selection
+		# if $flag321; then
+		# geotifcp -g $BAND10".txt"  $TILENAME".8bit.321.TIF" "$TILENAME.321.TIF"
+		# rm $TILENAME".8bit.321.TIF"
+		# # ############################################
+		# # #generate alpha information for nodata areas.  
+		# gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.321.TIF" "$TILENAME.321.alpha.TIF"
+		# fi
+		# 
+		# if $flag432; then
+		# geotifcp -g $BAND10".txt"  $TILENAME".8bit.432.TIF" "$TILENAME.432.TIF"
+		# rm $TILENAME".8bit.432.TIF"
+		# # ############################################
+		# # #generate alpha information for nodata areas.  
+		# gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.432.TIF" "$TILENAME.432.alpha.TIF"
+		# fi
+		# 
+		# if $flag543; then
+		# geotifcp -g $BAND10".txt"  $TILENAME".8bit.543.TIF" "$TILENAME.543.TIF"
+		# rm $TILENAME".8bit.543.TIF"
+		# # ############################################
+		# # #generate alpha information for nodata areas.  
+		# gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.543.TIF" "$TILENAME.543.alpha.TIF"
+		# fi
+		# 
+		# if $flag453; then
+		# geotifcp -g $BAND10".txt"  $TILENAME".8bit.453.TIF" "$TILENAME.453.TIF"
+		# rm $TILENAME".8bit.453.TIF"
+		# # ############################################
+		# # #generate alpha information for nodata areas.  
+		# gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.453.TIF" "$TILENAME.453.alpha.TIF"
+		# fi
+		# 
+		# if $flag754; then
+		# geotifcp -g $BAND10".txt"  $TILENAME".8bit.754.TIF" "$TILENAME.754.TIF"
+		# rm $TILENAME".8bit.754.TIF"
+		# # ############################################
+		# # #generate alpha information for nodata areas.  
+		# gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" "$TILENAME.754.TIF" "$TILENAME.754.alpha.TIF"
+		# fi
+	fi
+ fi
+
+## rm "321.TIF"
+## rm "432.TIF"
+## rm "543.TIF"
+## rm "453.TIF"
+## rm "754.TIF"
+# 
+
+
+# ############################################
+# 
+# 
+# 
+# 
+# 
+# 
 # #remove 0 0 0 rgb value and replace with transparency.
 # gdalwarp -dstalpha -srcnodata "0 0 0" -dstnodata "0 0 0" -co "TILED=YES" ${args[3]}'GEO.TIF' ${args[3]}'GEO_alpha.TIF'
 # #remove not georeferenced file
@@ -375,6 +626,8 @@ mv $TEMPDIR/"543.TIF" $INDEXDIR
 mv $TEMPDIR/"453.TIF" $INDEXDIR
 mv $TEMPDIR/"754.TIF" $INDEXDIR
 
+#delete temp directory
+
 rm -rf -- $TEMPDIR
 
 
@@ -385,7 +638,6 @@ echo "### Running gdal2tiles_openir.py ###"
 
 TIFFILES=($INDEXDIR/*)
 ARRAY=$(IFS=,; echo "[${TIFFILES[*]}]")
-
 
 ./gdal2tiles_openir.py ${ARRAY[*]} $INDEXDIR
 
